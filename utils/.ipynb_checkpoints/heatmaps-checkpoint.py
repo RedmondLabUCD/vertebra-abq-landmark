ffd18c31287fd66e0376f8af1707638d874daf9b
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-05-06 15:47:39 +0100

    Fix minor error

--- utils/.ipynb_checkpoints/heatmaps-checkpoint.py
+++ utils/.ipynb_checkpoints/heatmaps-checkpoint.py
@@ -33,6 +33,9 @@ def resize_coords(points, tl_dir, filename, vertebra):
     df = pd.read_csv(os.path.join(tl_dir,filename+".csv"))
     x_tl = df.iloc[vertebra,1]
     y_tl = df.iloc[vertebra,2]
+
+    print(points[:,0])
+    print(np.ones(len(scale_points[:,0]))*x_tl)
     
     scale_points[:,0] = points[:,0] - np.ones(len(scale_points[:,0]))*x_tl
     scale_points[:,1] = points[:,1] - np.ones(len(scale_points[:,0]))*y_tl
