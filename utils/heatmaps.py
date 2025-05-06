a8b4f1aecd93bf55c72626c191d4f4343a40c3d1
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-05-06 17:53:06 +0100

    Fix minor error

--- utils/heatmaps.py
+++ utils/heatmaps.py
@@ -34,10 +34,9 @@ def resize_coords(points, tl_dir, filename, vertebra):
     x_tl = df.iloc[vertebra,1]
     y_tl = df.iloc[vertebra,2]
 
-    print(points[:,0])
-    print(np.ones(len(scale_points[:,0]))*x_tl)
-
     points = np.array(points)
+
+    print(points)
     
     scale_points[:,0] = points[:,0] - np.ones(len(points[:,0]))*x_tl
     scale_points[:,1] = points[:,1] - np.ones(len(points[:,0]))*y_tl
