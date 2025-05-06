a26953d29d9b3999fea5a3cc8cdfa80ca0ce9601
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-05-06 17:44:54 +0100

    Fix minor error

--- utils/landmark_prep.py
+++ utils/landmark_prep.py
@@ -6,10 +6,11 @@ import itertools
 import math
 
 
-def resize_lm(landmarks,old_dim,new_dim):
+def resize_lm(points,old_dim,new_dim):
     '''
     Rescale landmarks to suit a change in the corresponding image size.
     '''
+    landmarks = np.array(points)
     
     landmarks[:,0] = landmarks[:,0] * new_dim/old_dim[0]
     landmarks[:,1] = landmarks[:,1] * new_dim/old_dim[1]
