98d7ade5bad60a08f1c3279f682f4718190ebd18
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-05-28 11:32:18 +0100

    minimize dataset size for initial testing

--- final_training.py
+++ final_training.py
@@ -90,16 +90,16 @@ def main():
     for index, row in csv_df.iterrows():
         image_name = row['image']
 
-        if index < int(0.8*len(csv_df)):
+        if index < int(0.05*len(csv_df)):
             train_over.append(image_name)
             train_id = row['id']
-        elif index < int(0.9*len(csv_df)):
+        elif index < int(0.1*len(csv_df)):
             if int(row['id']) == int(train_id):
                 train_over.append(image_name)
             else:
                 val_over.append(image_name)
                 val_id = row['id']
-        elif index >= int(0.9*len(csv_df)):
+        elif index >= int(0.95*len(csv_df)):
             if int(row['id']) == int(val_id):
                 val_over.append(image_name)
             else:
