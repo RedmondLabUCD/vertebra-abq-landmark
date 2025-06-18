8962cd3d781c9df4f493c535080f839fe6b6a85b
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-06-18 10:56:23 +0100

    prep for full model run

--- slurm_script_3.sh
+++ slurm_script_3.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --ntasks=1           ### How many CPU cores do you need?
 #SBATCH --mem=20G            ### How much RAM memory do you need?
-#SBATCH -p short          ### The queue to submit to: express, short, long, interactive
+#SBATCH -p short           ### The queue to submit to: express, short, long, interactive
 #SBATCH --gres=gpu:1         ### How many GPUs do you need?
 #SBATCH -t 0-24:00:00        ### The time limit in D-hh:mm:ss format
-#SBATCH -o /trinity/home/r094879/repositories/vertebra-identification/output/out0_%j.log       ### Where to store the console output (%j is the job number)
-#SBATCH -e /trinity/home/r094879/repositories/vertebra-identification/error/error0_%j.log      ### Where to store the error output
-#SBATCH --job-name=sp_lm_0  ### Name your job so you can distinguish between jobs
+#SBATCH -o /trinity/home/r094879/repositories/vertebra-abq-landmark/output/out_%j.log       ### Where to store the console output (%j is the job number)
+#SBATCH -e /trinity/home/r094879/repositories/vertebra-abq-landmark/error/error_%j.log      ### Where to store the error output
+#SBATCH --job-name=endplate  ### Name your job so you can distinguish between jcd .. obs
 #SBATCH --exclude=gpu004        ### exclude a gpu from the job
-#SBATCH --nodelist=gpu006       ### only use this gpu for the jobs
 
 # ----- Load the modules -----
 module purge
@@ -22,11 +21,12 @@ source "/tmp/${SLURM_JOB_USER}.${SLURM_JOB_ID}/prolog.env"
 source /trinity/home/r094879/vertebra-detection/bin/activate
 
 # ----- Your tasks -----
-# python final_training.py UNet_LM --ckpt "Checkpoint/Test2"
-# python final_training.py UNet_LM_CL3 --custom_loss True 
-# python final_training.py UNet_deep_CL3 --custom_loss True
-# python test.py UNet_LM_CL
-# python test.py UNet_LM_CL2
-# python test.py UNet_LM_CL3
+# python final_training.py UNet_ABQ_LM --custom_loss True --ckpt "Checkpoint/abq_lm/"
+# python final_training.py UNet_deep_CL2 --custom_loss True
+# python test.py UNet_LM_CL --ckpt "Checkpoint/Test1"
+# python test.py UNet_LM_CL2 --ckpt "Checkpoint/Test1"
+# python test.py UNet_LM_CL3 --ckpt "Checkpoint/Test1"
+# python test.py UNet_LM --ckpt "Checkpoint/Test2"
 
-python main.py
+# python main.py
+python final_training.py UNet_ABQ_LM_Full --custom_loss True --ckpt "Checkpoint/abq_lm_full/"
