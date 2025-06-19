42bcf66e06f6d2f9f495da0f8b755abb3a0d169c
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-06-19 15:35:57 +0100

    apply clahe

--- utils/roi_functions.py
+++ utils/roi_functions.py
@@ -41,9 +41,9 @@ def extract_ROI_from_lm(image_name,img,xy_pairs,image_size,dim=200,save_dir="/da
             # Define and save cropped ROI
             cropped_img_r = img[int(int(lm[i,1])-width):int(int(lm[i,1])+width),
                                 int(int(lm[i,0])-width):int(int(lm[i,0])+width)]
-            cv.imwrite(os.path.join(save_dir,image_name+"_"+str(i) +".png"),cropped_img_r)
+            # cv.imwrite(os.path.join(save_dir,image_name+"_"+str(i) +".png"),cropped_img_r)
 
-            img = cv.imread(os.path.join(save_dir,image_name+"_"+str(i) +".png"), 0)
+            # img = cv.imread(os.path.join(save_dir,image_name+"_"+str(i) +".png"), 0)
             clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
             img_clahe = clahe.apply(img)
             cv.imwrite(os.path.join(save_dir,image_name+"_"+str(i) +".png"),img_clahe)
