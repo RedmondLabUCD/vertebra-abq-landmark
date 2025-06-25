0948fa4c9036183fb4050832699d12046508087d
cgartland2 <cliodhna.gartland@ucdconnect.ie>
2025-06-25 14:52:15 +0100

    prep for testing of short model

--- utils/.ipynb_checkpoints/eval_metrics-checkpoint.py
+++ utils/.ipynb_checkpoints/eval_metrics-checkpoint.py
@@ -212,7 +212,7 @@ class curve_compare_metric_test(nn.Module):
         plt.legend()
         plt.title(f"Hausdorff Distance: {hd:.2f}")
         plt.gca().invert_yaxis()
-        plt.savefig(os.path.join('//data/scratch/r094879/data/Results',model_name,'output_heatmap_curve',+str(filename)+'.png'))
+        plt.savefig(os.path.join('//data/scratch/r094879/data/Results',model_name,'output_heatmap_curve',str(filename)+'.png'))
 
         return hd
 
